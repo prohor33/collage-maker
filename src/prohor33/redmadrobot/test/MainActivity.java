6cd189a10a2a34fac65d931de3a95fcfe81a2e83
Prohor <prohor33@gmail.com>
2013-10-27 00:50:06 +0400

    Not so bad, add process dialog and two views

--- src/prohor33/redmadrobot/test/MainActivity.java
+++ src/prohor33/redmadrobot/test/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.res.Configuration;
 import android.graphics.Bitmap;
 import android.util.Log;
@@ -19,6 +21,8 @@ import android.widget.RelativeLayout;
 public class MainActivity extends Activity {
 	
 	protected static Bitmap current_collage_preview;
+	protected static ProgressDialog progress_dialog;
+	protected static AsyncTask<String, Void, String> async_task;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -30,62 +34,106 @@ public class MainActivity extends Activity {
 			System.out.println("Create image view!");
 			CreateImageView(current_collage_preview);
 		}
+		
+		if (progress_dialog != null) {
+      progress_dialog = ProgressDialog.show(
+          MainActivity.this,
+          "In progress",
+          "Loading",
+          true,
+          true,
+          new DialogInterface.OnCancelListener(){
+              @Override
+              public void onCancel(DialogInterface dialog) {
+                  async_task.cancel(true);
+                  progress_dialog = null;
+              }
+          }
+      );		  
+		}
 				
-        final Button button = (Button) findViewById(R.id.GiveMeCollage);
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-            	System.out.println("button1 click");
-            	           	
+    final Button button = (Button) findViewById(R.id.GiveMeCollage);
+    button.setOnClickListener(new View.OnClickListener() {
+        public void onClick(View v) {
+        	System.out.println("button1 click");
+        	         
 
-	        	new AsyncTask<String, Void, String>(){
-	        		
-	        		Bitmap collage;
-	        		
-	        		  @Override
-	        		  protected String doInBackground(String... urlStr){        			  
-	        		    // do stuff on non-UI thread
-	        			
-	        			String exception_mess = new String();
-	        			CollageMaker collage_maker = new CollageMaker();
-	        			try {
-	        				collage = collage_maker.GimmeCollage(urlStr[0]);	        				
-	       				
-	        			} catch(IOException e) {
-	        				exception_mess = e.getMessage();
-	        			}
-	        			return exception_mess;
-	        		  }         
-	
-	        		  @Override
-	        		  protected void onPostExecute(String result){
-	        		    // do stuff on UI thread with the html
-	
-	        			 if (result.length() != 0) {
-	        				 messageBox("Internet Connection Problem", result);
-	        			 }
-	        			 else {
-	        				 
-	        				Bitmap preview;
-	         				if (collage.getWidth() > 450) { // too big
-	         					System.out.println("comress collgae to make preview");
-	         					float coef = 450.0f / collage.getWidth();
-	         					preview = Bitmap.createScaledBitmap(collage,
-	         							(int)(coef*collage.getWidth()), (int)(coef*collage.getHeight()), false);
-	         				}
-	         				else {
-	         					preview = collage;
-	         				}	        				 
-	         				current_collage_preview = preview;
-	         				
-	        				System.out.println("Start changing view...");
-	        				CreateImageView(preview);
 
-	        			 }
-	        		  }
-	        		}.execute("jennifer");
-            	
+      	new AsyncTask<String, Void, String>(){
+      		
+      		Bitmap collage;
+      		      		
+            @Override
+            protected void onPreExecute(){
+              
+              async_task = this;
+              
+              progress_dialog = ProgressDialog.show(
+                  MainActivity.this,
+                  "In progress",
+                  "Loading",
+                  true,
+                  true,
+                  new DialogInterface.OnCancelListener(){
+                      @Override
+                      public void onCancel(DialogInterface dialog) {
+                          async_task.cancel(true);
+                          progress_dialog = null;
+                      }
+                  }
+              );
+              
             }
-        });		
+      		
+      		  @Override
+      		  protected String doInBackground(String... urlStr){        			  
+      		    // do stuff on non-UI thread
+      		          		    
+      		    
+      			String exception_mess = new String();
+      			CollageMaker collage_maker = new CollageMaker();
+      			try {
+      				collage = collage_maker.GimmeCollage(urlStr[0]);	        				
+     				
+      			} catch(IOException e) {
+      				exception_mess = e.getMessage();
+      			}
+      			return exception_mess;
+      		  }         
+
+      		  @Override
+      		  protected void onPostExecute(String result){
+      		    // do stuff on UI thread with the html
+
+              progress_dialog.dismiss();
+              progress_dialog = null;      		    
+      		    
+      			 if (result.length() != 0) {
+      				 messageBox("Internet Connection Problem", result);
+      			 }
+      			 else {
+      				 
+      				Bitmap preview;
+       				if (collage.getWidth() > 450) { // too big
+       					System.out.println("comress collgae to make preview");
+       					float coef = 450.0f / collage.getWidth();
+       					preview = Bitmap.createScaledBitmap(collage,
+       							(int)(coef*collage.getWidth()), (int)(coef*collage.getHeight()), false);
+       				}
+       				else {
+       					preview = collage;
+       				}	        				 
+       				current_collage_preview = preview;
+       				
+      				System.out.println("Start changing view...");
+      				CreateImageView(preview);
+
+      			 }
+      		  }
+      		}.execute("jennifer");
+        	
+        }
+    });		
 	}
 
 	@Override
@@ -114,7 +162,9 @@ public class MainActivity extends Activity {
 	  
 	  int orientation = MainActivity.this.getResources().getConfiguration().orientation;
 	  
-    RelativeLayout rl = (RelativeLayout) findViewById(R.id.RelativeLayout01);	  
+    RelativeLayout rl = (RelativeLayout) findViewById(R.id.RelativeLayout01);
+    
+    int image_view_id = 91; // random number (for simplicity)
     
     // create new "Send By Email" button
     Button bt = new Button(MainActivity.this);                  
@@ -125,17 +175,23 @@ public class MainActivity extends Activity {
         RelativeLayout.LayoutParams.WRAP_CONTENT,
         RelativeLayout.LayoutParams.WRAP_CONTENT);
     
-    lp3.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);    
-    lp3.setMargins(0, 0, 0, 20);
-    lp3.addRule(RelativeLayout.CENTER_HORIZONTAL);
+    lp3.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);   
+    
+    if (orientation == Configuration.ORIENTATION_PORTRAIT) {
+      lp3.addRule(RelativeLayout.CENTER_HORIZONTAL);
+      lp3.setMargins(0, 0, 0, 20);
+    }
+    else {
+      lp3.addRule(RelativeLayout.ALIGN_RIGHT, image_view_id);
+      lp3.setMargins(0, 0, 50, 20);
+    }
     rl.addView(bt, lp3);	  
   	  
     
   	// create image view
   	ImageView iv = new ImageView(MainActivity.this);
   	iv.setImageBitmap(image_preview);		
-  	
-  	int image_view_id = 91;	// random number (for simplicity)
+  	 	
   	iv.setId(image_view_id);
   	
   	RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(
@@ -168,17 +224,24 @@ public class MainActivity extends Activity {
   	else {
   	  lp2.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
   	  lp2.setMargins(10, 0, 0, 0);
+  	  et.setEms(9);
+  	  et.setTextSize(20);
   	}
   	et.setLayoutParams(lp2);
 			
 		
+  	// edit button "Give Me Collage"
+  	Button gmc_button = (Button) findViewById(R.id.GiveMeCollage);  
+  	gmc_button.setText("Give Me Another One");
+  	gmc_button.setTextSize(15);
+  	
 	}
 	
   @Override
   public void onBackPressed() {    
-    System.out.println("DemoActivity::onBackPressed()");
+    System.out.println("DemoActivity::onBackPressed()");    
     // reset view
-    current_collage_preview = null;
+    current_collage_preview = null;    
     super.onBackPressed();
   }
   
