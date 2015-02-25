06e52a89314668633d92b143da067afe3d42334a
Gladkikh Prohor <prohor33@gmail.com>
2015-02-26 01:24:43 +0300

    add project files

---
+++ app/src/main/java/ndk/asteroids/prohor/collage_maker/app/MainActivity.java
@@ -0,0 +1,99 @@
+package ndk.asteroids.prohor.collage_maker.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import ndk.asteroids.prohor.collage_maker.R;
+import ndk.asteroids.prohor.collage_maker.collage_maker.CollageMaker;
+import ndk.asteroids.prohor.collage_maker.instagram_api.InstagramAPI;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        InstagramAPI.putContext(MainActivity.this);
+
+        final EditText nickEditText = (EditText) findViewById(R.id.editText);
+
+        // TODO: remove
+        nickEditText.setText("damedvedev");
+
+        Button mainButton = (Button) findViewById(R.id.mainButton);
+        mainButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String nickname = nickEditText.getText().toString();
+                if (nickname.isEmpty()) {
+                    Toast.makeText(getApplicationContext(), getString(R.string.no_nickname_text),
+                            Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                InstagramAPI.with(new InstagramAPI.Listener() {
+                    @Override
+                    public void onSuccess() {
+                        getUserMedia();
+                    }
+
+                    @Override
+                    public void onFail(String error) {
+                        Toast.makeText(getApplicationContext(), error,
+                                Toast.LENGTH_LONG).show();
+                    }
+                }).findUser(nickname);
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    void getUserMedia() {
+        InstagramAPI.with(new InstagramAPI.Listener() {
+            @Override
+            public void onSuccess() {
+                CollageMaker.generateCollage();
+
+                // debug
+                Toast.makeText(getApplicationContext(), "successfully find user",
+                        Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onFail(String error) {
+                Toast.makeText(getApplicationContext(), error,
+                        Toast.LENGTH_LONG).show();
+            }
+        }).fetchUserMedia();
+    }
+}
