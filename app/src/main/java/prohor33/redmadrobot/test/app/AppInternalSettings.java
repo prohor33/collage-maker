35fc7fc101cb7df1080a837a4cc870a1d8577d77
Gladkikh Prohor <prohor33@gmail.com>
2015-03-04 17:58:09 +0300

    back to debug version

--- app/src/main/java/prohor33/redmadrobot/test/app/AppInternalSettings.java
+++ app/src/main/java/prohor33/redmadrobot/test/app/AppInternalSettings.java
@@ -4,7 +4,7 @@ package prohor33.redmadrobot.test.app;
  * Created by prohor on 03/03/15.
  */
 public class AppInternalSettings {
-    public static boolean isReleaseVersion = true;
+    public static boolean isReleaseVersion = false;
     public static boolean showAds = isReleaseVersion;
     public static boolean collectStatistics = isReleaseVersion;
 }
